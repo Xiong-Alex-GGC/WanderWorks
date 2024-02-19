0f8dec090a5c614b9bae67ee8d74090bd1cb1615
jportillo5 <jtsportillo@gmail.com>
2024-02-19 12:00:36 -0500

    Backend Finished for transportation and accommodations

--- backend/controllers/transportationController.js
+++ backend/controllers/transportationController.js
@@ -1,15 +1,86 @@
 // controllers/transportationController.js
 import * as transportationModel from '../models/transportationModel.js';
 
+export const getAllTransportations = async (req, res) => {
+  try {
+    const list = await transportationModel.getAllTransportations(); //create a list of all the activity documents in the collection
+    res.send(list); //res is essentially a return statement
+  } catch (error) {
+    console.error('Error fetching Transportation data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const getItineraryTransportations = async (req, res) => {
+  try {
+
+    const list = await transportationModel.getAllItineraryTransportations(req.params.id);
+    res.send(list);
+  } catch (error) {
+    console.error('Error fetching Itinerary data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
 export const createTransportation = async (req, res) => {
-    try {
-      const data = req.body;
-      console.log("Data of Transportation ", data);
+  try {
+    const data = req.body;
+    console.log("Data of Transportation ", data);
   
-      await transportationModel.createTransportation(data);
-      res.send({ msg: "Transportation Added" });
-    } catch (error) {
-      console.error('Error creating Transportation data:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    await transportationModel.createTransportation(data);
+    res.send({ msg: "Transportation Added" });
+  } catch (error) {
+    console.error('Error creating Transportation data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const updateTransportation = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const data = req.body;
+
+    await transportationModel.updateTransportation(id, data);
+    res.send({ msg: "Transportation Updated" });
+  } catch (error) {
+    console.error('Error updating Transportation data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+//retrieves an activity by its Id
+export const getTransportationById = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const transportationData = await transportationModel.getTransportationById(id);
+
+    if (transportationData) {
+      res.json(transportationData);
+    } else {
+      res.status(404).json({ error: 'Transportation not found' });
     }
-  };
+  } catch (error) {
+    console.error('Error fetching specific Transportation data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+//deleteActivity
+export const deleteTransportation = async (req, res) => {
+  const userID = req.body.userID; 
+  const transportationID = req.body.transportationID;
+
+  try {
+    const deletionResult = await transportationModel.deleteTransportation(transportationID);
+
+    if (deletionResult) {
+      res.send({ msg: "Transportation Deleted" });
+    } else {
+      res.status(404).json({ error: 'Transportation not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting Transportation data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- backend/models/accommodationModel.js
+++ backend/models/accommodationModel.js
@@ -3,7 +3,40 @@ import db from '../firebaseConfig.js';
 
 const accommodationCollection = collection(db, 'Accommodation'); //connecting to the database by specifying table name
 
+export const getAllAccommodations = async () => { //get is, well, getting; post is writing to the database
+  const snapshot = await getDocs(accommodationCollection); //get current snapshot of the collection
+  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })); //returns the data to whatever is calling it
+};
+
+export const getAllItineraryAccommodations = async (itinID) => {
+  const queryItin = query(accommodationCollection, where('itineraryID', '==', itinID));
+  const snapshot = await getDocs(queryItin);
+  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+};
 
 export const createAccommodation = async (data) => {
-    await addDoc(accommodationCollection, data); //add document to the collection
-  };
+  await addDoc(accommodationCollection, data); //add document to the collection
+};
+
+export const updateAccommodation = async (id, data) => {
+await updateDoc(doc(accommodationCollection, id), data); //update a pre-existing document
+};
+
+export const getAccommodationById = async (id) => {
+  const docSnapshot = await getDoc(doc(accommodationCollection, id));
+  return docSnapshot.exists() ? { id: docSnapshot.id, ...docSnapshot.data() } : null;
+};
+
+
+export const deleteAccommodation = async (accommodationID) => {
+
+  //checks if activity exists
+  const accommodationDoc = await getDoc(doc(accommodationCollection, accommodationID));
+
+  if (accommodationDoc.exists()) {
+    await deleteDoc(doc(accommodationCollection, accommodationID));
+    return true; // Deletion successful
+  }
+
+  return false; // Transportation not found 
+};
