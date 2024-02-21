f3f322e9b8f2858b0994008a6b3e0602774dc734
Xiong-Alex <axiong14@ggc.edu>
2024-02-21 00:14:26 -0500

    Address auto-suggests Location & Address

---
+++ frontend/src/components/Mapbox/ItineraryLocationSuggestion.js
@@ -0,0 +1,77 @@
+import mapboxToken from '../../tokens/mapboxToken';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ItineraryLocationSuggestion = ({ onSuggestionSelect  }) => {
+  const [address, setAddress] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleInputChange = (event) => {
+    const inputAddress = event.target.value;
+    setAddress(inputAddress);
+
+    // Call Mapbox Geocoding API for auto-suggestions
+    if (inputAddress.trim() !== '') {
+      fetchSuggestions(inputAddress);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const fetchSuggestions = async (input) => {
+    try {
+      const response = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          input
+        )}.json?access_token=` + mapboxToken
+      );
+
+      // Extract suggestions from the response with specified place types
+      const filteredSuggestions = response.data.features.filter(
+        (feature) =>
+          ['country', 'region', 'province', 'state', 'district', 'place'].includes(
+            feature.place_type[0]
+          )
+      );
+
+      const suggestionsList = filteredSuggestions.map((feature) => feature.place_name);
+      setSuggestions(suggestionsList);
+    } catch (error) {
+      console.error('Error fetching suggestions:', error);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    onSuggestionSelect(suggestion);
+    setAddress(suggestion);
+    setSuggestions([]);
+  };
+
+  return (
+    <div>
+      <label>Location (Required)</label>
+      <br />
+      <input
+        type="text"
+        value={address}
+        onChange={handleInputChange}
+        placeholder="Enter your address"
+        required
+      />
+      <br />
+
+      {/* Display suggestions */}
+      {suggestions.length > 0 && (
+        <ul>
+          {suggestions.map((suggestion, index) => (
+            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ItineraryLocationSuggestion;
