05b27d9741302da446b9835e9133b267b83bf515
jportillo5 <jtsportillo@gmail.com>
2024-03-29 13:52:35 -0400

    Itinerary Confines enforced

---
+++ backend/errors/OverlappingItemError.js
@@ -0,0 +1,7 @@
+class OverlappingItemError extends Error { //used to prevent overlapping Plan-A activities and possibly accommodations?
+    constructor(message) {
+        super(message);
+        this.name = 'OverlappingItemError';
+        this.statusCode = 400; // HTTP status code for bad request
+    }
+}
--- backend/models/accommodationModel.js
+++ backend/models/accommodationModel.js
@@ -1,8 +1,44 @@
 import { addDoc, collection, getDocs, updateDoc, doc, getDoc, query, where, deleteDoc } from 'firebase/firestore';
 import db from '../firebaseConfig.js';
 import * as itineraryController from '../controllers/itineraryController.js';
+import * as itineraryModel from './itineraryModel.js';
 
 const accommodationCollection = collection(db, 'Accommodation'); //connecting to the database by specifying table name
+
+export const extractDate = (dateString) => {
+  return dateString.split('T')[0];
+}
+
+function segmentExtractedDate(dateString) {
+  return dateString.split('-');
+}
+
+function compareDates(date1, date2) { //returns -1 if date1 is before date2, 0 if date1 and date2 are the same, and 1 if date1 is after date2
+  //extracted date format: YYYY-MM-DD
+  const date1Split = segmentExtractedDate(date1);
+  const date2Split = segmentExtractedDate(date2);
+  if(date1Split[0] == date2Split[0]) { //years are equal
+    if(date1Split[1] == date2Split[1]) { //months are equal
+      if(date1Split[2] == date2Split[2]) { //days are equal
+        return 0; //dates match up perfectly
+      } else if(date1Split[2] < date2Split[2]) { //day1 is before day2
+        return -1;
+      } else { //day 1 is after day2
+        return 1;
+      }
+    } else if(date1Split[1] < date2Split[1]) { //date1's month is before date2's
+      return -1;
+    } else { //month1 is after month2
+      return 1;
+    }
+  } else if(date1Split[0] < date2Split[0]) { //date1's year is before date2's
+    return -1;
+  } else { //year1 is after year2
+    return 1;
+  }
+}
+
+//function isAccommodationOverlapping(existingAccommodations, newAccommodation)
 
 export const getAllAccommodations = async () => { //get is, well, getting; post is writing to the database
   const snapshot = await getDocs(accommodationCollection); //get current snapshot of the collection
@@ -17,12 +53,28 @@ export const getAllItineraryAccommodations = async (itinID) => {
 
 export const createAccommodation = async (data) => {
   //ensure the accommodation is within the confines of the itinerary start and end dates
+  const itinID = data.itineraryID;
+  const itinData = await itineraryModel.getItineraryById(itinID);
+  const itinStartDate = extractDate(itinData.startDate);
+  const itinEndDate = extractDate(itinData.endDate);
+  const accStartDate = extractDate(data.startDate);
+  const accEndDate = extractDate(data.endDate);
 
-  //ensure the accommodation does not overlap with other accommodations (for some reason)
+  //ensure accStartDate is not before itinStartDate
+  if(compareDates(accStartDate, itinStartDate) == -1) {
+    throw new ItemOutOfBoundsError("Accommodation cannot start before your trip's start date.");
+  }
+
+  //ensure accEndDate is not after itinEndDate
+  if(compareDates(accEndDate, itinEndDate) == 1) {
+    throw new ItemOutOfBoundsError("Accommodation cannot extend beyond the duration of your trip.");
+  }
+  //ensure the accommodation does not overlap with other accommodations
+  //some people may opt to stay at one hotel for the start of a trip, and stay at a different hotel for the rest, so this is
+  //a scenario we should probably account for
 
 
   //throw an error if one of the fail conditions above is met, otherwise complete request
-  const itinID = data.itineraryID;
   await itineraryController.addExpenses(itinID, data.expenses); 
   await addDoc(accommodationCollection, data); //add document to the collection
 };
